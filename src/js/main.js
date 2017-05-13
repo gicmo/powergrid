1d81ffa1d71ee986bab27bed0665c258086dd984
Christian Kellner <christian@kellner.me>
2017-05-13 19:15:53 +0200

    @spa move router/index.js into main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,26 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
-
 import VueResource from 'vue-resource'
+import Router from 'vue-router'
+
+import App from './App'
+import RunTable from '@/js/components/RunTable'
+
+Vue.use(Router)
 
 Vue.use(VueResource)
 Vue.config.productionTip = false
+
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Overview',
+      component: RunTable
+    }
+  ]
+})
 
 /* eslint-disable no-new */
 new Vue({
